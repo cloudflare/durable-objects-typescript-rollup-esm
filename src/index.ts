0d02d7e492096f5b4068ee4a5c7b2224bdcf094b
Alaa Zorkane <Alaazorkane@gmail.com>
2021-08-09 04:26:26 +0100

    Make odd check faster

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import isOdd from 'is-odd'
-
 // In order for the workers runtime to find the class that implements
 // our Durable Object namespace, we must export it from the root module.
 export { CounterTs } from './counter'
@@ -19,7 +17,7 @@ async function handleRequest(request: Request, env: Env) {
   let obj = env.COUNTER.get(id)
   let resp = await obj.fetch(request.url)
   let count = parseInt(await resp.text())
-  let wasOdd = isOdd(count) ? 'is odd' : 'is even'
+  let wasOdd = (count & 1) === 1 ? 'is odd' : 'is even'
 
   return new Response(`${count} ${wasOdd}`)
 }
