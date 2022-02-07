9e820b5c58d1a2a06f07d5553b78007f4a35a06f
Cody Koeninger <cody@koeninger.org>
2022-02-07 15:36:43 -0600

    Apply kenton's comments from base template PR

--- src/counter.ts
+++ src/counter.ts
@@ -9,15 +9,17 @@ export class CounterTs {
   async fetch(request: Request) {
     // Apply requested action.
     let url = new URL(request.url);
+
+    // Durable Object storage is automatically cached in-memory, so reading the
+    // same key every request is fast. (That said, you could also store the
+    // value in a class member if you prefer.)
     let value: number = await this.state.storage?.get("value") || 0;
     switch (url.pathname) {
     case "/increment":
       ++value;
-      await this.state.storage?.put("value", value);
       break;
     case "/decrement":
       --value;
-      await this.state.storage?.put("value", value);
       break;
     case "/":
       // Just serve the current value. No storage calls needed!
@@ -26,6 +28,12 @@ export class CounterTs {
       return new Response("Not found", {status: 404});
     }
 
+    // We don't have to worry about a concurrent request having modified the
+    // value in storage because "input gates" will automatically protect against
+    // unwanted concurrency. So, read-modify-write is safe. For more details,
+    // see: https://blog.cloudflare.com/durable-objects-easy-fast-correct-choose-three/
+    await this.state.storage?.put("value", value);
+      
     return new Response(value.toString());
   }
 }
