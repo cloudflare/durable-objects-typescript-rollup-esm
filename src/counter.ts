2c6c4bf710c56a8beb708a58483c3a1e44515fc6
Alex Robinson <arobinson@cloudflare.com>
2022-02-06 21:24:38 -0600

    Take advantage of i/o gates for counter logic

--- src/counter.ts
+++ src/counter.ts
@@ -1,30 +1,23 @@
 export class CounterTs {
-  value: number = 0
   state: DurableObjectState
 
   constructor(state: DurableObjectState, env: Env) {
     this.state = state;
-    // `blockConcurrencyWhile()` ensures no requests are delivered until
-    // initialization completes.
-    this.state.blockConcurrencyWhile(async () => {
-        let stored = await this.state.storage?.get<number>("value");
-        this.value = stored || 0;
-    })
   }
 
   // Handle HTTP requests from clients.
   async fetch(request: Request) {
     // Apply requested action.
     let url = new URL(request.url);
-    let currentValue = this.value;
+    let value: number = await this.state.storage?.get("value") || 0;
     switch (url.pathname) {
     case "/increment":
-      currentValue = ++this.value;
-      await this.state.storage?.put("value", this.value);
+      ++value;
+      await this.state.storage?.put("value", value);
       break;
     case "/decrement":
-      currentValue = --this.value;
-      await this.state.storage?.put("value", this.value);
+      --value;
+      await this.state.storage?.put("value", value);
       break;
     case "/":
       // Just serve the current value. No storage calls needed!
@@ -33,12 +26,7 @@ export class CounterTs {
       return new Response("Not found", {status: 404});
     }
 
-    // Return `currentValue`. Note that `this.value` may have been
-    // incremented or decremented by a concurrent request when we
-    // yielded the event loop to `await` the `storage.put` above!
-    // That's why we stored the counter value created by this
-    // request in `currentValue` before we used `await`.
-    return new Response(currentValue.toString());
+    return new Response(value.toString());
   }
 }
 
